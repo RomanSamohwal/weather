f421b623c3d5114c9e87232970cc0a5c73f9f9f7
RomanSamohwal <https://github.com/RomanSamohwal>
2021-03-09 00:14:43 +0300

    fix minWidth

--- src/components/temperature/TemperatureInfo.tsx
+++ src/components/temperature/TemperatureInfo.tsx
@@ -4,7 +4,7 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
 export const TemperatureInfo = (props: any) => {
-    return <Card style={{margin: '10px', width: '20vw', display: 'flex', justifyContent: 'center'}}>
+    return <Card style={{margin: '10px', minWidth: '20vw', display: 'flex', justifyContent: 'center'}}>
         <CardContent>
             <Typography variant='body2' color='textSecondary' component='p'>
                 {props.name}
